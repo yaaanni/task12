cdc63f5a4bde2392634f4cfc0da4064723c30efe
yaaanni <rw.yana.rw@gmail.com>
2024-09-22 23:52:46 +0300

    c

---
+++ Task_12.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+int main() {
+double a,b,c,x,t,d,t1,t2,x1,x2,x3,x4;
+std::cout << "Введите  a: ";
+std::cin >> a;
+std::cout << "Введите  b: ";
+std::cin >> b;
+std::cout << "Введите  c: ";
+std::cin >> c;
+t = x*x;
+d = sqrt(b*b-4*a*c);
+if (d<0) {
+    std::cout << "error";
+} else {
+t1 = (d-b)/(2*a);
+t2 = (-d-b)/(2*a);
+if (t1 < 0) {
+x1 = sqrt(t2);
+x2 = -sqrt(t2);
+std::cout << "Значение первого корня уравнения: " << x1 << std::endl;
+std::cout << "Значение второго корня уравнения: " << x2 << std::endl;
+} else if (t2<0) {
+x1 = sqrt(t1);
+x2 = -sqrt(t1);
+std::cout << "Значение первого корня уравнения: " << x1 << std::endl;
+std::cout << "Значение второго корня уравнения: " << x2 << std::endl;
+} else {
+    x1 = sqrt(t1);
+x2 = -sqrt(t1);
+std::cout << "Значение первого корня уравнения: "<< x1 << std::endl;
+std::cout << "Значение второго корня уравнения: "<< x2 << std::endl;
+x1 = sqrt(t2);
+x2 = -sqrt(t2);
+std::cout << "Значение третьего корня уравнения: "<<x1 << std::endl;
+std::cout << "Значение четвертого корня уравнения: "<<x2 << std::endl;
+}
+}
+}
